6ac2e16e34793211b53478ca7ca939d29f190e57
Marko Cirovic <marko.cirovic@yahoo.com>
2023-03-01 21:17:16 +0100

    remove h1 and add new p tag in index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<h1>Hello World!!!</h1>'; ?>
     <?php echo '<p>Cao Cirko, ovo ide iz lokala!</p>'; ?>
+    <?php echo '<p>Cao Stefke kralju!</p>'; ?>
   </body>
 </html>
